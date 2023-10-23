a1833d9367625bcfea59636da66056cc65c2ecbb
Nabillah <nabillahnurhaliza@if.unjani.ac.id>
2023-10-23 11:33:00 +0700

    Create Order Class

---
+++ srp/Order.java
@@ -0,0 +1,25 @@
+class Order {
+    void calculateTotalSum() {
+        /*...*/
+    }
+    void getItems() {
+        /*...*/
+    }
+    void getItemCount() {
+        /*...*/
+    }
+    void addItem() {
+        /*...*/
+    }
+    void deleteItem() {
+        /*...*/
+    }
+    void printOrder() {
+        /*...*/
+    }
+    void showOrder() {
+        /*...*/
+    }
+    void getDailyHistory() {}
+    void getMonthlyHistory() {}
+}
