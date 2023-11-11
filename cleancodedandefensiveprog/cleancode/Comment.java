c8a6b97bbf0ad8ac3a50a1e6992ce6004b40d956
Nabillah <nabillahnurhaliza@if.unjani.ac.id>
2023-11-11 21:25:44 +0700

    create good Comment class

--- cleancodedandefensiveprog/cleancode/Comment.java
+++ cleancodedandefensiveprog/cleancode/Comment.java
@@ -1,20 +1,15 @@
 package cleancode;
+import java.util.regex.Pattern;
 
 public class Comment {
-    /**
-* Default constructor.
-*/
-protected AnnualDateRule() { }
-
-    /** The day of the month. */
-    private int dayOfMonth;
-
-    /**
-     * Returns the day of the month.
-     * 
-     * @return the day of the month.
-     */
-    public int getDayOfMonth() {
-        return dayOfMonth;
-    }
+    // Copyright (C) 2011 by Osoco. All rights reserved.
+    // Released under the terms of the GNU General Public License
+    // version 2 or later.
+    // Returns an instance of the Responder being tested.
+    protected abstract Responder responderInstance();
+    // renaming the function: responderBeingTested
+    // format matched kk:mm:ss EEE, MMM dd, yyyy
+    Pattern timeMatcher = Pattern.compile("\\d*:\\d*:\\d* \\w*, \\w* \\d*,\\d*");
+    assertTrue(a.compareTo(b) == -1); // a < b
+    assertTrue(b.compareTo(a) == 1); // b > a
 }
