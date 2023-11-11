e3d120bb011fe3273edacd7e9bca80818adcd090
Nabillah <nabillahnurhaliza@if.unjani.ac.id>
2023-11-11 21:43:46 +0700

    change FitNesseExpediter class

---
+++ cleancodedandefensiveprog/cleancode/FitNesseExpediter.java
@@ -0,0 +1,18 @@
+package cleancode;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+
+public class FitNesseExpediter implements ResponseSender {
+    private Socket socket;
+    private InputStream input;
+    private OutputStream output;
+    private Request request;
+    private Response response;
+    private FitNesseContext context;
+    protected long requestParsingTimeLimit;
+    private long requestProgress;
+    private long requestParsingDeadLine;
+    private boolean hasError;
+}
