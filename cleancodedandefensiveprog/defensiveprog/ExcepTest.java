9691840a27efe9202d295f4000b9b295ebd83346
Nabillah <nabillahnurhaliza@if.unjani.ac.id>
2023-11-11 23:23:20 +0700

    change and compile ExcepTest.java (the finally block)

--- cleancodedandefensiveprog/defensiveprog/ExcepTest.java
+++ cleancodedandefensiveprog/defensiveprog/ExcepTest.java
@@ -1,27 +1,17 @@
 package defensiveprog;
-
 import java.io.*;
 
 public class ExcepTest {
     public static void main(String args[]) {
+        int a[] = new int[2];
         try {
-            int a[] = new int[2];
             System.out.println("Access element three :" + a[3]);
         } catch (ArrayIndexOutOfBoundsException e) {
             System.out.println("Exception thrown :" + e);
-        }
-        System.out.println("Out of the block");
-        try {
-            String fileName = "example.txt";
-            FileInputStream file = new FileInputStream(fileName);
-            byte x = (byte) file.read();
-        }catch (FileNotFoundException f) {
-            f.printStackTrace();
-            System.exit(-1);
-        } catch (IOException i) {
-            i.printStackTrace();
-            System.exit(-1);
-        System.out.println("Out of the second block");
+        } finally {
+            a[0] = 6;
+            System.out.println("First element value: " + a[0]);
+            System.out.println("The finally statement is executed");
         }
     }
 }
